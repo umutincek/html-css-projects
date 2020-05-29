7547292c251a1673c1d7932aeed1b6fc2943b036
Umut İncek <61050017+umutincek@users.noreply.github.com>
2020-05-29 20:00:58 +0300

    Portfolio
    Portfolio site for Html and Css

---
+++ portfolio/script.js
@@ -0,0 +1,10 @@
+function open_myMenu() {
+    document.getElementById('myMenu').style.width="60%";
+    document.getElementById('myMenu').style.display="block";
+    document.getElementById('sag').style.display="none";
+}
+
+function close_myMenu() {
+    document.getElementById('myMenu').style.display="none";
+    document.getElementById('sag').style.display="block";
+}
